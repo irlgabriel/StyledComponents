a01aeca5f19e781d66f56391536e46920092fb09
Gabriel Radu <radu.gabriel131@gmail.com>
2020-09-26 15:30:28 +0300

    added footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { FooterContainer, FooterSubHeading, FooterSubscription, FooterSubText } from "./Footer.elements"
+import { Link } from "react-router-dom"
+import { FooterLinksContainer, FooterLinksWrapper, FooterLinksItem, FooterLinkTitle, FooterLink, FooterContainer, FooterSubHeading, FooterSubscription, FooterSubText, Form, FormInput } from "./Footer.elements"
+import { Button } from "../../globalStyles";
+
 const Footer = () => {
   return (
     <>
@@ -9,9 +12,57 @@ const Footer = () => {
             Join our exclusive membership to receive the latest news and trends
           </FooterSubHeading>
           <FooterSubText>
-
+            You can unsubscribe at any time.
           </FooterSubText>
+            <Form>
+              <FormInput name="email" type="email" placeholder="Your Email" />
+                <Button fontBig>Subscribe</Button>
+            </Form>
         </FooterSubscription>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinksItem>
+              <FooterLinkTitle>
+                About Us
+              </FooterLinkTitle>
+              <FooterLink to='/sign-up'>How It Works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinksItem>
+            <FooterLinksItem>
+              <FooterLinkTitle>
+                Contact Us
+              </FooterLinkTitle>
+              <FooterLink to='/sign-up'>How It Works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinksItem>
+            <FooterLinksItem>
+              <FooterLinkTitle>
+                Videos
+              </FooterLinkTitle>
+              <FooterLink to='/sign-up'>How It Works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinksItem>
+            <FooterLinksItem>
+              <FooterLinkTitle>
+                Social Media
+              </FooterLinkTitle>
+              <FooterLink to='/sign-up'>How It Works</FooterLink>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinksItem>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
       </FooterContainer>
     </>
   )
