4cc18d840c82889f8d469a1f31c1630cd591dabc
Gabriel Radu <radu.gabriel131@gmail.com>
2020-09-26 14:58:57 +0300

    Styled front page!

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavItemBtn,
+  NavBtnLink,
+} from "./Navbar.elements";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "../../globalStyles";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <NavIcon />
+            ULTRA
+          </NavLogo>
+          <MobileIcon onClick={handleClick} click={click}>
+            {click ? <FaTimes /> : <FaBars />}
+          </MobileIcon>
+          <NavMenu click={click} onClick={handleClick}>
+            <NavItem>
+              <NavLinks to="/">Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/products">Products</NavLinks>
+            </NavItem>
+            <NavItemBtn>
+              {button ? (
+                <NavBtnLink to="/sign-up">
+                  <Button primary>SIGN UP</Button>
+                </NavBtnLink>
+              ) : (
+                <NavBtnLink to="/sign-up">
+                  <Button fontBig primary>
+                    SIGN UP
+                  </Button>
+                </NavBtnLink>
+              )}
+            </NavItemBtn>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
