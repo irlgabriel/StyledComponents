14bb96d9613cb7c6d694363392a51c6459c79425
Gabriel Radu <radu.gabriel131@gmail.com>
2020-09-26 15:59:07 +0300

    Fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -29,85 +29,89 @@ import {
 } from "./Footer.elements";
 import { Button } from "../../globalStyles";
 
-const Footer = () => {
+function Footer() {
   return (
-    <>
-      <FooterContainer>
-        <FooterSubscription>
-          <FooterSubHeading>
-            Join our exclusive membership to receive the latest news and trends
-          </FooterSubHeading>
-          <FooterSubText>You can unsubscribe at any time.</FooterSubText>
-          <Form>
-            <FormInput name="email" type="email" placeholder="Your Email" />
-            <Button fontBig>Subscribe</Button>
-          </Form>
-        </FooterSubscription>
-        <FooterLinksContainer>
-          <FooterLinksWrapper>
-            <FooterLinksItem>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/sign-up">How It Works</FooterLink>
-              <FooterLink to="/">Testimonials</FooterLink>
-              <FooterLink to="/">Careers</FooterLink>
-              <FooterLink to="/">Investors</FooterLink>
-              <FooterLink to="/">Terms of Service</FooterLink>
-            </FooterLinksItem>
-            <FooterLinksItem>
-              <FooterLinkTitle>Contact Us</FooterLinkTitle>
-              <FooterLink to="/sign-up">How It Works</FooterLink>
-              <FooterLink to="/">Testimonials</FooterLink>
-              <FooterLink to="/">Careers</FooterLink>
-              <FooterLink to="/">Investors</FooterLink>
-              <FooterLink to="/">Terms of Service</FooterLink>
-            </FooterLinksItem>
-            <FooterLinksItem>
-              <FooterLinkTitle>Videos</FooterLinkTitle>
-              <FooterLink to="/sign-up">How It Works</FooterLink>
-              <FooterLink to="/">Testimonials</FooterLink>
-              <FooterLink to="/">Careers</FooterLink>
-              <FooterLink to="/">Investors</FooterLink>
-              <FooterLink to="/">Terms of Service</FooterLink>
-            </FooterLinksItem>
-            <FooterLinksItem>
-              <FooterLinkTitle>Social Media</FooterLinkTitle>
-              <FooterLink to="/sign-up">How It Works</FooterLink>
-              <FooterLink to="/">Testimonials</FooterLink>
-              <FooterLink to="/">Careers</FooterLink>
-              <FooterLink to="/">Investors</FooterLink>
-              <FooterLink to="/">Terms of Service</FooterLink>
-            </FooterLinksItem>
-          </FooterLinksWrapper>
-        </FooterLinksContainer>
-        <SocialMedia>
-          <SocialMediaWrap>
-            <SocialLogo to="/">
-              <SocialIcon />
-              ULTRA
-            </SocialLogo>
-            <WebsiteRights>ULTRA &copy; 2020</WebsiteRights>
-            <SocialIcons>
-              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
-                <FaFacebook />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
-                <FaInstagram />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
-                <FaYoutube />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
-                <FaTwitter />
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
-                <FaLinkedin />
-              </SocialIconLink>
-            </SocialIcons>
-          </SocialMediaWrap>
-        </SocialMedia>
-      </FooterContainer>
-    </>
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name='email' type='email' placeholder='Your Email' />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinksItem>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to='/sign-up'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinksItem>
+          <FooterLinksItem>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to='/'>Contact</FooterLink>
+            <FooterLink to='/'>Support</FooterLink>
+            <FooterLink to='/'>Destinations</FooterLink>
+            <FooterLink to='/'>Sponsorships</FooterLink>
+          </FooterLinksItem>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinksItem>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to='/'>Submit Video</FooterLink>
+            <FooterLink to='/'>Ambassadors</FooterLink>
+            <FooterLink to='/'>Agency</FooterLink>
+            <FooterLink to='/'>Influencer</FooterLink>
+          </FooterLinksItem>
+          <FooterLinksItem>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to='/'>Instagram</FooterLink>
+            <FooterLink to='/'>Facebook</FooterLink>
+            <FooterLink to='/'>Youtube</FooterLink>
+            <FooterLink to='/'>Twitter</FooterLink>
+          </FooterLinksItem>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to='/'>
+            <SocialIcon />
+            ULTRA
+          </SocialLogo>
+          <WebsiteRights>ULTRA © 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              rel='noopener noreferrer'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='LinkedIn'>
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
   );
-};
+}
 
 export default Footer;
